1f265dab7ccd43fd6c103184ac616ff46094d71a
Christopher Scalcucci <cscalcucci@whitestar.io>
2022-11-04 07:40:16 -0400

    Added UITableViewCell and UICollectionViewCell

--- Sources/Roark/Base/AsyncOperation.swift
+++ Sources/Roark/Base/AsyncOperation.swift
@@ -7,36 +7,73 @@
 
 import Foundation
 
+fileprivate extension NSLock {
+    func withCriticalScope<T>(_ block: () -> T) -> T {
+        lock()
+        let value = block()
+        unlock()
+        return value
+    }
+}
+
 public typealias AsyncCompletion = ((AsyncOperation.State) -> Void)
 
 open class AsyncOperation: Operation {
     public typealias StateOperationBlock = (@escaping (AsyncOperation.State) -> Void) -> Void
 
     public enum State: String {
-        case ready, executing, finished
+        case ready = "isReady"
+        case executing = "isExecuting"
+        case finished = "isFinished"
+    }
 
-        fileprivate var keyPath: String {
-            "is\(rawValue.capitalized)"
+    /// Private storage for the `state` property that will be KVO observed.
+    private var _state : State = .ready
+
+    /// A lock to guard reads and writes to the `_state` property
+    private let stateLock = NSLock()
+
+    private var state : State {
+        get {
+            return stateLock.withCriticalScope({ _state })
+        } set {
+            let oldValue : State = self.stateLock.withCriticalScope({ _state })
+            guard newValue != oldValue else { return }
+
+            /*
+             It's important to note that the KVO notifications are NOT called from inside
+             the lock. If they were, the app would deadlock, because in the middle of
+             calling the `didChangeValueForKey()` method, the observers try to access
+             properties like "isReady" or "isFinished". Since those methods also
+             acquire the lock, then we'd be stuck waiting on our own lock. It's the
+             classic definition of deadlock.
+             */
+            willChangeValue(forKey: oldValue.rawValue)
+            willChangeValue(forKey: newValue.rawValue)
+
+            stateLock.withCriticalScope({
+                guard _state != .finished else { return }
+
+                _state = newValue
+            })
+
+            didChangeValue(forKey: oldValue.rawValue)
+            didChangeValue(forKey: newValue.rawValue)
         }
     }
 
-    public var state = State.ready {
-        willSet {
-            willChangeValue(forKey: newValue.keyPath)
-            willChangeValue(forKey: state.keyPath)
-        }
-        didSet {
-            didChangeValue(forKey: oldValue.keyPath)
-            didChangeValue(forKey: state.keyPath)
-        }
+    public override var isReady: Bool {
+        return state == .ready
     }
 
     public override var isExecuting: Bool {
-        state == .executing
+        return state == .executing
     }
 
     public override var isFinished: Bool {
-        state == .finished
+        let currentState = state
+        if isCancelled && currentState != .executing { return true }
+        return currentState == .finished
     }
 
     public override var isAsynchronous: Bool {
@@ -44,11 +81,7 @@ open class AsyncOperation: Operation {
     }
 
     public override func start() {
-        guard !isCancelled else {
-            state = .finished
-            return
-        }
-        state = .executing
+        guard !isCancelled else { state = .finished; return }
         main()
     }
 
@@ -68,6 +101,8 @@ open class AsyncOperation: Operation {
     }
 
     public override func main() {
+        state = .executing
+
         self.block({ [weak self] state in
             guard let self = self else { return }
             self.state = state
---
+++ Sources/Roark/RKKit/Views/RKCollectionViewCell.swift
@@ -0,0 +1,88 @@
+//
+//  RKCollectionViewCell.swift
+//  
+//
+//  Created by Chris Scalcucci on 11/4/22.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+open class RKCollectionViewCell<VM: RKCellViewModel> : UICollectionViewCell {
+
+    //
+    // MARK: Life Cycle Properties
+    //
+
+    open var viewModel : VM?
+
+    private var completedInitialSetup : Bool = false
+
+    public var disposeBag = DisposeBag()
+
+    //
+    // MARK: Creation
+    //
+
+    open class func create<T: RKCollectionViewCell>(vm: VM) -> T {
+        let vc = T()
+        vc.setModel(vm)
+        return vc
+    }
+
+    open class func `init`(vm: VM) -> Self {
+        let cell = self.init()
+        cell.setModel(vm)
+        return cell
+    }
+
+    @discardableResult
+    open func setModel(_ vm: VM?) -> Self {
+        _ = self
+
+        // We have no view model
+        if self.viewModel == nil {
+            // And are now setting one
+            if vm != nil {
+                self.viewModel = vm
+
+                // Have we setup our views yet?
+                if !self.completedInitialSetup {
+                    self.completedInitialSetup = true
+                    setupViews()
+                }
+
+                setupViewBindings()
+            }
+        } else if vm == nil {
+            // Remove all bindings
+            self.disposeBag = DisposeBag()
+            self.viewModel = nil
+        } else if vm != self.viewModel {
+            // Someone forgot to prepare for reuse, we are setting over with a new VM
+            self.viewModel = vm
+            setupViewBindings()
+        }
+
+        return self
+    }
+
+    //
+    // MARK: LifeCycle
+    //
+
+    @objc open func setupViewBindings() {
+
+    }
+
+    @objc open func setupViews() {
+
+    }
+
+    open override func prepareForReuse() {
+        super.prepareForReuse()
+
+        self.setModel(nil)
+    }
+}
